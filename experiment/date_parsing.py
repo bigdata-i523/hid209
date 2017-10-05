72e5082fe074917206e4351d9eb20cd2b20c9523
wxhan0128 <han105330@gmail.com>
2017-10-05 00:41:41 -0400

    finished date_parsing.py

--- experiment/date_parsing.py
+++ experiment/date_parsing.py
@@ -4,161 +4,196 @@ import glob
 import dateparser
 import yaml
 
-# all variables/counters declaration
-path = 'notebook*.md'  # find all notebook files in the current directory
-files = glob.glob(path)
-file_number = 1
-
-dict = {}
-dict["Logistic"] = {}
-dict["Logistic"]["Date"] = []
-dict["Logistic"]["Content"] = []
-dict["Theory"] = {}
-dict["Theory"]["Date"] = []
-dict["Theory"]["Content"] = []
-dict["Practice"] = {}
-dict["Practice"]["Date"] = []
-dict["Practice"]["Content"] = []
-dict["Writing"] = {}
-dict["Writing"]["Date"] = []
-dict["Writing"]["Content"] = []
-dict["Meetings"] = {}
-dict["Meetings"]["Date"] = []
-dict["Meetings"]["Content"] = []
-
-find_logistic = False
-find_theory = False
-find_practice = False
-find_writing = False
-find_meetings = False
-
-counter = 0
-
 
 # convert the input data format
 def convert_data(input_lines):
     output_lines = []
+    cur_sections = []
+
     for new_line in input_lines:
         new_line = new_line.replace("*", "-")
-        new_line = new_line.replace("# ", "")
+
+        if new_line.startswith("# "):
+            new_line = new_line.replace("# ", "")
+            cur_sections.append(new_line.replace("\n", ""))
+
+        new_line = new_line.replace("Weeks with no progress", "Inactive")
         new_line = new_line.replace("\t", "    ")
         output_lines.append(new_line)
 
-    return output_lines
+    return output_lines, cur_sections
+
+
+def extract_data(read_lines):
+    counter = 0
+    flag = None
+
+    # initialize a dictionary (table)
+    dict = {}
+    for i in section_list:
+        dict[i] = {}
+        dict[i]["Date"] = []
+        dict[i]["Content"] = []
+
+    # Loop through all the lines
+    for read_line in read_lines:
+        # search for titles
+        if read_line.startswith("Logistic\n"):
+            flag = "Logistic"
+            continue
+        elif read_line.startswith("Theory\n"):
+            flag = "Theory"
+            continue
+        elif read_line.startswith("Practice\n"):
+            flag = "Practice"
+            continue
+        elif read_line.startswith("Writing\n"):
+            flag = "Writing"
+            continue
+        elif read_line.startswith("Meetings\n"):
+            flag = "Meetings"
+            continue
+
+        # read the data between two '\n's
+        if flag == "Logistic":
+            if read_line != "\n":
+                split_str(dict[flag], read_line)
+            if read_line.startswith("\n"):
+                counter = counter + 1
+        elif flag == "Theory":
+            if read_line != "\n":
+                split_str(dict[flag], read_line)
+            if read_line.startswith("\n"):
+                counter = counter + 1
+        elif flag == "Practice":
+            if read_line != "\n":
+                split_str(dict[flag], read_line)
+            if read_line.startswith("\n"):
+                counter = counter + 1
+        elif flag == "Writing":
+            if read_line != "\n":
+                split_str(dict[flag], read_line)
+            if read_line.startswith("\n"):
+                counter = counter + 1
+        elif flag == "Meetings":
+            if read_line != "\n":
+                split_str(dict[flag], read_line)
+            if read_line.startswith("\n"):
+                counter = counter + 1
+
+        # when counter=2, means it has reached to another title
+        if counter == 2:
+            counter = 0
+            flag = None
+
+    return dict
 
 
 # split each line of data into {content:[], date[]} format
-def split_str(title, input_line):
+def split_str(row, content):
     # if there exist a range of time
-    if input_line[11:12] == "-":
-        date1 = dateparser.parse(input_line[2:10]).strftime("%y%m%d")
-        date2 = dateparser.parse(input_line[13:21]).strftime("%y%m%d")
+    if content[11:12] == "-":
+        date1 = dateparser.parse(content[2:10]).strftime("%y%m%d")
+        date2 = dateparser.parse(content[13:21]).strftime("%y%m%d")
         period = date1 + date2
-        dict[title]["Date"].append(period)  # include two dates, which is a period
+        row["Date"].append(period)  # include two dates, which is a period
 
-        content = input_line[22:(len(input_line) - 1)].replace("\n", "")
-        dict[title]["Content"].append(content)
+        content = content[22:(len(content) - 1)].replace("\n", "")
+        row["Content"].append(content)
 
     # no range of time, just a single date
     else:
-        cur_date = dateparser.parse(input_line[2:10]).strftime(
+        cur_date = dateparser.parse(content[2:10]).strftime(
             "%y%m%d")  # for a single date, there are at most 8 characters
-        dict[title]["Date"].append(cur_date)
+        row["Date"].append(cur_date)
 
-        content = input_line[11:(len(input_line) - 1)].replace("\n", "")
-        dict[title]["Content"].append(content)
+        content = content[11:(len(content) - 1)].replace("\n", "")
+        row["Content"].append(content)
 
 
-def clear_lists(titles):
-    for title in titles:
-        dict[title]["Date"] = []
-        dict[title]["Content"] = []
+def check_sections(name, sections):
+    missed_sections = set(section_list) - set(sections)
+    if missed_sections:
+        print(name + " misses such sections:")
+        for i in missed_sections:
+            print(i)
+    else:
+        print(name + " does not miss any section.")
+
+
+def check_attend(name, meetings):
+    missed_meetings = set(meeting_list) - set(meetings)
+    if missed_meetings:
+        print(name + " misses such meetings:")
+        for i in missed_meetings:
+            print(i)
+    else:
+        print(name + " does not miss any meeting.")
+
+
+def output_data(in_data, file_name):
+    # write file with yaml data format
+    stream = file(file_name, "w")
+    yaml.dump(in_data, stream)  # write a yaml representation of data to 'data_parsing.yaml'
+    print(yaml.dump(in_data))
+
+
+def display_section_date(dictionary, name):
+    if name:
+        if name not in section_list:
+            print("Section name is wrong.")
+        else:
+            print(dictionary[name]["Date"])
+    else:
+        for i in section_list:
+            print(dictionary[i]["Date"])
+
+
+def display_all_info(dictionary):
+    print(dictionary)
+    print(dictionary["Logistic"])
+    print(dictionary["Theory"])
+    print(dictionary["Practice"])
+    print(dictionary["Writing"])
+    print(dictionary["Meetings"])
 
 
 if __name__ == '__main__':
+    result = {}
+
+    # all variables/counters declaration
+    path = 'notebook*.md'  # find all notebook files in the current directory
+    files = glob.glob(path)
+    file_number = 1
+
+    # global variables
+    section_list = ["Logistic", "Theory", "Practice", "Writing", "Meetings"]  # this is for complete notebook file
+    meeting_list = ['170825', '170901', '170908', '170915', '170922']  # all meetings that student need to attend
+
     for my_file in files:
         try:
-            print(my_file)
+            print(my_file + "\n")
 
             # read notebook*.md files
             with open(my_file, "r") as f:
                 read_lines = f.readlines()
-                converted_lines = convert_data(read_lines)
+                (converted_lines, my_sections) = convert_data(read_lines)
                 # print(converted_lines)
 
-                # Loop through all the lines
-                for converted_line in converted_lines:
-                    # search for titles
-                    if converted_line.startswith("Logistic\n"):
-                        find_logistic = True
-                        continue
-                    elif converted_line.startswith("Theory\n"):
-                        find_theory = True
-                        continue
-                    elif converted_line.startswith("Practice\n"):
-                        find_practice = True
-                        continue
-                    elif converted_line.startswith("Writing\n"):
-                        find_writing = True
-                        continue
-                    elif converted_line.startswith("Meetings\n"):
-                        find_meetings = True
-                        continue
+                result = extract_data(converted_lines)
 
-                    # read the data between two '\n's
-                    if find_logistic:
-                        if converted_line != "\n":
-                            split_str("Logistic", converted_line)
-                        if converted_line.startswith("\n"):
-                            counter = counter + 1
-                    elif find_theory:
-                        if converted_line != "\n":
-                            split_str("Theory", converted_line)
-                        if converted_line.startswith("\n"):
-                            counter = counter + 1
-                    elif find_practice:
-                        if converted_line != "\n":
-                            split_str("Practice", converted_line)
-                        if converted_line.startswith("\n"):
-                            counter = counter + 1
-                    elif find_writing:
-                        if converted_line != "\n":
-                            split_str("Writing", converted_line)
-                        if converted_line.startswith("\n"):
-                            counter = counter + 1
-                    elif find_meetings:
-                        if converted_line != "\n":
-                            split_str("Meetings", converted_line)
-                        if converted_line.startswith("\n"):
-                            counter = counter + 1
+                # display_all_info(result)
 
-                    # when counter=2, means it has reached to another title
-                    if counter == 2:
-                        counter = 0
-                        find_logistic = False
-                        find_theory = False
-                        find_practice = False
-                        find_writing = False
-                        find_meetings = False
+                section_name = "Meetings"
+                display_section_date(result, section_name)
 
-                print(dict)
-                # print(dict["Logistic"])
-                # print(dict["Theory"])
-                # print(dict["Practice"])
-                # print(dict["Writing"])
-                # print(dict["Meetings"])
+                check_sections(my_file, my_sections)
+                check_attend(my_file, result["Meetings"]["Date"])
 
                 # define the output file name (yaml file), correspond to input file name
                 out_str = 'data_parsing_' + my_file + '.yaml'
-                print(out_str)
-
-                # write file with yaml data format
-                stream = file(out_str, "w")
-                yaml.dump(dict, stream)  # write a yaml representation of data to 'data_parsing.yaml'
-                print(yaml.dump(dict))
-
-                # clear all sub-lists for the new file
-                clear_lists(["Logistic", "Theory", "Practice", "Writing", "Meetings"])
+                print("\n---------------------------------" + out_str + " data---------------------------------")
+                output_data(result, out_str)
 
             file_number = file_number + 1
 
